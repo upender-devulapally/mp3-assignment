caedef15bbfa9cadb8bfcf96e0273b70055bb723
Upender <upender.devulapally@gmail.com>
2013-07-08 23:32:15 +0530

    publi index.html deleted and root url added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Mp3Assignment::Application.routes.draw do
   resources :songs
-
-
+  root :to => 'songs#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
