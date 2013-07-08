4db2c9405912ee72dc896827abec6c748ea29a55
Upender <upender.devulapally@gmail.com>
2013-07-08 20:52:58 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mp3Assignment::Application.config.secret_token = 'f6e98dc17f543263544f2e84c8371713fdbb543373610efaa56888864dae2f3eb5b3e92357f9f74d6d16b6d4f6dc3ff6706d59b3763a6ec1e94abb25ffbce3b1'
